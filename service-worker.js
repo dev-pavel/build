fbe78b383fe5f3688a31f7d28b4355cfaac1648d
OnlocDev <105296052+dev-pavel@users.noreply.github.com>
2022-10-31 23:51:39 -0230

    .

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2250741e856ea4d9477e93ef52e2e32f.js"
+  "/precache-manifest.a72bebfe139a01c43d898b97a17d55e1.js"
 );
 
 workbox.clientsClaim();
